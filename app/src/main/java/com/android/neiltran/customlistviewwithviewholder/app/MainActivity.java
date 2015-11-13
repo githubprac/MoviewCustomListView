5052f984caa3604d5f05a1cccd042430d36e4a91
Neil.Tran <neil.tran@infonam.com>
2015-11-13 17:54:11 +0700

    [Thuan] init project

---
+++ app/src/main/java/com/android/neiltran/customlistviewwithviewholder/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.android.neiltran.customlistviewwithviewholder.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.neiltran.customlistviewwithviewholder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
